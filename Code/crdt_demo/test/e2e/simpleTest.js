bdbe950ce3d819066abfaf2d8e839b81bacb4734
Patrick Reuter <patrickreuter@outlook.com>
2017-05-11 02:08:29 +0200

    Wrote more tests

--- Code/crdt_demo/test/e2e/simpleTest.js
+++ Code/crdt_demo/test/e2e/simpleTest.js
@@ -5,38 +5,43 @@ module.exports = { // adapted from: https://git.io/vodU0
     browser
       .url('http://10.200.1.63:3000/')
       .waitForElementVisible('body')
-      .waitForElementVisible('button:nth-child(1)', 1000)
-      .click('button:nth-child(1)')
-      .pause(1000)
-      .saveScreenshot('screenshots/crdtTest.png')
-      .end();
-  },
 
-  'Adding something to the textfield': function(browser) {
+    var loops = 10
+    for(var i=0;i<loops;i++){
+      browser.click('#incrementCounter').pause(50)
+    }
 
     browser
-      .url('http://10.200.1.63:3000/')
-      .waitForElementVisible('body')
-      .setValueAndTrigger('.addShoppingItemField:nth-child(1)', 'nightwatch')
       .pause(1000)
-      .waitForElementVisible('.incrementButtonAddItemBtn:nth-child(1)')
-      .click('button:nth-child(1)')
+  },
+
+  // 'Write to the Textfield': function(browser) {
+  //   var loops = 10
+  //   for (var i=0;i<loops;i++){
+  //     browser
+  //       .setValue('#addItemField', 'nightwatch ' + i)
+  //       .pause(200)
+  //       .click('#addItemBtn')
+  //   }
+  //   browser
+  //     .pause(1000)
+  // },
+
+  'Trigger the Toggle': function(browser) {
+      browser
+      .waitForElementVisible('#myToggle')
+        .click('#myToggle').pause(100)
+        .click('#incrementCounter').pause(50)
+        .pause(500)
+
+    // var loops = 10
+    // for (var i=0;i<loops;i++){
+    //   browser
+    //     .click('#myToggle')
+    //     .pause(500)
+    // }
+    browser
       .pause(1000)
-      .saveScreenshot('screenshots/crdtTest.png')
       .end();
   }
 };
-//
-
-//
-// //Write elements in the set
-// var tf = document.getElementsByClassName("addShoppingItemField")[0];
-// var addBtn = document.getElementsByClassName("addItemBtn")[0];
-// var executions = 10
-//
-// for(var i=0;i<executions;i++){
-//     tf.value = "Product "+i;
-//     var event = new Event('input', { bubbles: true });
-//     tf.dispatchEvent(event)
-//     addBtn.click()
-// }
