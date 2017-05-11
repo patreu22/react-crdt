5f97a2e419996bbd6cbabd30c210a224d042ee7b
Patrick Reuter <patrickreuter@outlook.com>
2017-05-11 13:45:52 +0200

    Moved Nightwatch to standalone node project

---
+++ Code/testclient_server/node_modules/selenium-download/lib/index.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2014, Groupon, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of GROUPON nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+'use strict';
+var fs = require('fs');
+var path = require('path');
+
+var mkdirp = require('mkdirp');
+var async = require('async');
+
+var tempdir = require('./tempdir');
+var ensureSelenium = require('./selenium');
+var ensureChromedriver = require('./chromedriver');
+
+var TEMP_PATH = tempdir + 'testium';
+var CHROMEDRIVER = 'chromedriver';
+var SELENIUM_JAR = 'selenium.jar';
+
+var BINARIES = [CHROMEDRIVER, SELENIUM_JAR];
+
+function makePaths(binPath, tempPath) {
+  mkdirp.sync(binPath);
+  return mkdirp.sync(tempPath);
+}
+
+function cleanupDir(dir) {
+  function deleteIfExists(filename) {
+    try {
+      fs.unlinkSync(path.join(dir, filename));
+    } catch (err) {
+      if (err.code !== 'ENOENT') throw err;
+    }
+  }
+
+  BINARIES.forEach(deleteIfExists);
+}
+
+function binariesExist(binPath) {
+  return BINARIES.every(function binaryExists(binary) {
+    return fs.existsSync(binPath + '/' + binary);
+  });
+}
+
+function ensure(binPath, callback) {
+  if (binariesExist(binPath)) {
+    return callback();
+  }
+  makePaths(binPath, TEMP_PATH);
+  return async.parallel([
+    ensureSelenium(binPath, TEMP_PATH),
+    ensureChromedriver(binPath, TEMP_PATH)
+  ], callback);
+}
+
+function update(binPath, callback) {
+  cleanupDir(binPath);
+  return ensure(binPath, callback);
+}
+
+function forceUpdate(binPath, callback) {
+  cleanupDir(binPath);
+  cleanupDir(TEMP_PATH);
+  return ensure(binPath, callback);
+}
+
+module.exports = {
+  update: update,
+  forceUpdate: forceUpdate,
+  ensure: ensure
+};
---
+++ Code/testclient_server/test/e2e/simpleTest.js
@@ -0,0 +1,52 @@
+var config = require('../../nightwatch.conf.BASIC.js');
+
+module.exports = { // adapted from: https://git.io/vodU0
+  'User behaviour 1': function(browser) {
+    browser.url('http://10.200.1.63:3000/')
+    .waitForElementVisible('body').pause(100)
+  },
+
+  'Increment the Counter': function(browser) {
+    browser
+      .url('http://10.200.1.63:3000/')
+      .waitForElementVisible('body')
+      .click('#myToggle').pause(100)
+
+    var loops = 10
+    for(var i=0;i<loops;i++){
+      browser.click('#incrementCounter').pause(50)
+    }
+
+    browser
+      .pause(1000)
+  },
+
+  'Write to the Textfield': function(browser) {
+    var loops = 10
+    for (var i=0;i<loops;i++){
+      browser
+        .setValue('#addItemField', 'nightwatch ' + i)
+        .pause(200)
+        .click('#addItemBtn')
+    }
+    browser
+      .pause(1000)
+  },
+
+  'Trigger the Toggle': function(browser) {
+      browser
+      .waitForElementVisible('#myToggle')
+
+    loops = 9
+    for(var i=0;i<loops;i++){
+      browser.execute(function() {
+        var toggle = document.getElementById("myToggle")
+        toggle.click()
+      }).pause(100)
+    }
+    browser
+      .pause(1000)
+      .end();
+  }
+
+};
